0d7a0193c5b6730f17d650a36af9a0f3f02a8101
Emir <124746082+ELEMIR71@users.noreply.github.com>
2023-02-28 21:59:16 -0300

    Add files via upload
    utilizado para darle funcionalidad a la pagina ( no logrado)

---
+++ auxiliar/auxiliar.js
@@ -0,0 +1,21 @@
+   /** const boton1=document.getElementById(nombre)
+   boton1.addEventListener('onmouseover',() =>{
+    document.getElementById("demo").innerHTML = "Hi, My name is Brett Cox"
+    } )
+    document.addEventListener.onmouseover, function () {
+        document.body.style('display',inline-flex )
+      }
+      **/
+document.getElementById("nombre").onmouseenter = function () {
+document.getElementById("demo").innerHTML="otra ves lrpm"  
+document.getElementById("name").style.display = "inline-flex";
+
+}
+document.getElementById("nombre").onmouseleave = function () {
+    document.getElementById("demo").innerHTML=""  
+    document.getElementById("name").style.display = "none"; }
+
+/**document.addEventListener('nombre').addEventListener('onmouseenter', function () {
+    document.getElementById('name').style.display = 'inline-flex';
+  })
+**/
